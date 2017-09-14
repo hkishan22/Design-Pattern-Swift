498709d66241635340f5ec2e994f6f4c00404e5c
hari Kishan <h.kishan22@gmail.com>
2017-09-14 12:22:19 +0530

    Creational Design Pattern added

---
+++ Creational Design Pattern/Factory Design Pattern.playground/Contents.swift
@@ -0,0 +1,101 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Abstract Class
+protocol Car {
+    
+    func type() -> CarTypes
+    func seatAvailble() -> Int
+    
+    func features()->String
+}
+
+
+// types of available Cars
+enum CarTypes  {
+    case HatchBack , Sedan, Suv
+}
+
+
+// Classes inherit from same Abstaract Class . Or using Same Protocol
+class HatchBack: Car {
+
+    func type() -> CarTypes {
+        return .HatchBack
+    }
+    
+    func seatAvailble() -> Int {
+        return 5
+    }
+    
+    func features()->String {
+        return "Small car with 5 seating capabilites, Good for a small family for  city drive"
+    }
+}
+
+
+class Suv: Car {
+    
+    func type() -> CarTypes {
+        return .Suv
+    }
+    
+    func seatAvailble() -> Int {
+        return 7
+    }
+    
+    func features()->String {
+        return "Big car with 7 seating capabilites, Good for big families"
+    }
+}
+
+
+class Sedan: Car {
+    
+    func type() -> CarTypes {
+        return .Sedan
+    }
+    
+    func seatAvailble() -> Int {
+        return 6
+    }
+    
+    func features()->String {
+        return "Big car with 6 seating capabilites, comfortable for long journey"
+    }
+}
+
+
+// Factory Class
+class CarFactory {
+    
+    static func makeCar(type:CarTypes)-> Car {
+
+        switch type {
+        case .HatchBack:
+            return HatchBack()
+
+        case .Suv:
+            return Suv()
+
+        case .Sedan:
+            return Sedan()
+        }
+    }
+}
+
+
+// Using Factory Class to get New Car
+let  newSedan  = CarFactory.makeCar(type: .Sedan)
+    print(newSedan.seatAvailble())
+    print(newSedan.type())
+    print(newSedan.features())
+
+
+let  newHatchback  = CarFactory.makeCar(type: .HatchBack)
+print(newHatchback.seatAvailble())
+print(newHatchback.type())
+print(newHatchback.features())
+
+
